fba028fef52b6d097da8a117f12b267e40a6515a
jeongkite <jeong6kite@gmail.com>
2021-08-31 23:35:14 +0900

    12번 질문이 왜 나오지 않을까? 시작하기 버튼은 왜 단번에 되지 않을까?

--- music/views.py
+++ music/views.py
@@ -2,8 +2,11 @@ from music.models import Answer, Mbti, Question, Result
 import music
 from django.shortcuts import get_object_or_404, redirect, render
 from random import randint
+import hashlib
+import time
 
 # Create your views here.
+QUESTION_COUNT = 12
 
 
 def main(request):
@@ -17,53 +20,69 @@ def more(request):
 def start(request):
     question = get_object_or_404(Question, q_number=1)
     answers = question.answer_set.all()
-    this_mbti = Mbti(order=2, e_i=0, s_n=0, t_f=0, p_j=0)
+    this_mbti = Mbti(order=1, e_i=0, s_n=0, t_f=0, p_j=0)
+
+    # temp = str(customer.pk + int(time.time()))
+    # encoded = temp.encode()
+    # result = hashlib.sha256(encoded).hexdigest()
+    # customer.hash_code = result
+
     this_mbti.save()
     ctx = {
         'question': question,
         'answers': answers,
-        'this_user': this_mbti.pk
+        'this_user': this_mbti.pk,
+        'black': range(0),
+        'gray': range(11),
     }
     return render(request, 'music/question.html', context=ctx)
 
 
 def question(request, this_user):
-    choice = request.POST['choice']
     this_mbti = get_object_or_404(Mbti, pk=this_user)
-
     question = get_object_or_404(Question, q_number=this_mbti.order)
     answers = question.answer_set.all()
-    this_mbti.order += 1
 
-    if choice == 'e':
-        this_mbti.e_i += 1
-    elif choice == 'i':
-        this_mbti.e_i -= 1
-    elif choice == 's':
-        this_mbti.s_n += 1
-    elif choice == 'n':
-        this_mbti.s_n -= 1
-    elif choice == 't':
-        this_mbti.t_f += 1
-    elif choice == 'f':
-        this_mbti.t_f -= 1
-    elif choice == 'p':
-        this_mbti.p_j += 1
-    elif choice == 'j':
-        this_mbti.p_j -= 1
-    else:
-        print('mbti select error!')
+    if request.method == "POST":
+        choice = request.POST['choice']
+        this_mbti.order += 1
 
-    this_mbti.save()
-    print(question)
+        if choice == 'e':
+            this_mbti.e_i += 1
+        elif choice == 'i':
+            this_mbti.e_i -= 1
+        elif choice == 's':
+            this_mbti.s_n += 1
+        elif choice == 'n':
+            this_mbti.s_n -= 1
+        elif choice == 't':
+            this_mbti.t_f += 1
+        elif choice == 'f':
+            this_mbti.t_f -= 1
+        elif choice == 'p':
+            this_mbti.p_j += 1
+        elif choice == 'j':
+            this_mbti.p_j -= 1
+        else:
+            print('mbti select error!')
+
+        this_mbti.save()
+        print(question)
+
+    qn = question.q_number
+    black_circle = qn - 1
+    print(black_circle)
+    gray_circle = QUESTION_COUNT - black_circle - 1
 
     ctx = {
         'question': question,
         'answers': answers,
-        'this_user': this_mbti.pk
+        'this_user': this_mbti.pk,
+        'black': range(black_circle),
+        'gray': range(gray_circle),
     }
 
-    if this_mbti.order == 13:
+    if this_mbti.order >= 13:
         return redirect('music:end', this_mbti.pk)
     else:
         return render(request, 'music/question.html', context=ctx)
@@ -75,6 +94,9 @@ def end_story(request, this_user):
 
 def calc_result(request, this_user):
     this_mbti = get_object_or_404(Mbti, pk=this_user)
+
+    if this_mbti.order < 13:
+        return redirect('music:question', this_mbti.pk)
 
     result = ""
     if this_mbti.e_i > 0:
@@ -123,5 +145,11 @@ def calc_result(request, this_user):
 
     print(result)
     rslt_content = get_object_or_404(Result, mbti=result)
+    result_texts = rslt_content.mbtitext_set.all()
 
-    return render(request, 'music/result.html', {'result': rslt_content})
+    ctx = {
+        'result': rslt_content,
+        'texts': result_texts,
+    }
+
+    return render(request, 'music/result.html', ctx)
