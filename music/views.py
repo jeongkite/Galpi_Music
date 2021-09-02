70efd7718d45d6b5e4278d9ba872a97e2e1688b0
jeongkite <jeong6kite@gmail.com>
2021-09-02 20:16:57 +0900

    hash_code 적용, ul 구분자 없이

--- music/views.py
+++ music/views.py
@@ -20,26 +20,31 @@ def more(request):
 def start(request):
     question = get_object_or_404(Question, q_number=1)
     answers = question.answer_set.all()
-    this_mbti = Mbti(order=1, e_i=0, s_n=0, t_f=0, p_j=0)
+    this_mbti = Mbti(order=2, e_i=0, s_n=0, t_f=0, p_j=0)
+    this_mbti.save()
 
-    # temp = str(customer.pk + int(time.time()))
-    # encoded = temp.encode()
-    # result = hashlib.sha256(encoded).hexdigest()
-    # customer.hash_code = result
+    temp = str(this_mbti.pk + int(time.time()))
+    encoded = temp.encode()
+    result = hashlib.sha256(encoded).hexdigest()
+    this_mbti.hash_code = result
 
     this_mbti.save()
     ctx = {
         'question': question,
         'answers': answers,
-        'this_user': this_mbti.pk,
+        'code': this_mbti.hash_code,
         'black': range(0),
         'gray': range(11),
     }
     return render(request, 'music/question.html', context=ctx)
 
 
-def question(request, this_user):
-    this_mbti = get_object_or_404(Mbti, pk=this_user)
+def question(request, code):
+    this_mbti = get_object_or_404(Mbti, hash_code=code)
+
+    if this_mbti.order >= 13:
+        return redirect('music:end', this_mbti.hash_code)
+
     question = get_object_or_404(Question, q_number=this_mbti.order)
     answers = question.answer_set.all()
 
@@ -71,32 +76,28 @@ def question(request, this_user):
 
     qn = question.q_number
     black_circle = qn - 1
-    print(black_circle)
     gray_circle = QUESTION_COUNT - black_circle - 1
 
     ctx = {
         'question': question,
         'answers': answers,
-        'this_user': this_mbti.pk,
+        'code': this_mbti.hash_code,
         'black': range(black_circle),
         'gray': range(gray_circle),
     }
 
-    if this_mbti.order >= 13:
-        return redirect('music:end', this_mbti.pk)
-    else:
-        return render(request, 'music/question.html', context=ctx)
+    return render(request, 'music/question.html', context=ctx)
 
 
-def end_story(request, this_user):
-    return render(request, 'music/endstory.html', {'this_user': this_user})
+def end_story(request, code):
+    return render(request, 'music/endstory.html', {'code': code})
 
 
-def calc_result(request, this_user):
-    this_mbti = get_object_or_404(Mbti, pk=this_user)
+def calc_result(request, code):
+    this_mbti = get_object_or_404(Mbti, hash_code=code)
 
     if this_mbti.order < 13:
-        return redirect('music:question', this_mbti.pk)
+        return redirect('music:question', this_mbti.hash_code)
 
     result = ""
     if this_mbti.e_i > 0:
